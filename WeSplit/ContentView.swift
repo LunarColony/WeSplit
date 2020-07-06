d10fb6196c7c50aabb0e578c8dbde08265ca32c4
LunarColony <yunisfarah4@gmail.com>
2020-07-06 15:51:26 +0100

    Added tip calculation section and tweaked some properties and their values

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -15,6 +15,22 @@ struct ContentView: View {
 
     @State private var tipPercentages = [10, 15, 20, 25, 0]
 
+    
+   var totalPerson: Double {
+        let peopleCount = Double(numberOfPeople + 1)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = ( orderAmount / 100 ) * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
+    }
+    
+    
+    
+    
+    
     var body: some View {
         NavigationView {
             Form {
@@ -23,19 +39,23 @@ struct ContentView: View {
                         .keyboardType(.decimalPad)
 
                     Picker("Number of people", selection: $numberOfPeople) {
-                        ForEach(1..<51) {
+                        ForEach(1..<100) {
                             Text("\($0) people")
                         }
                     }
                 }
 
                 Section(header: Text("How much do you want to leave?")) {
-                    Picker("Tip percentage", selection: $tipPercentages) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
                         ForEach(0..<tipPercentages.count) {
                             Text("\(self.tipPercentages[$0])%")
                         }
                     }
                         .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section {
+                    Text("$\(totalPerson, specifier: "%.2f")")
                 }
 
 
